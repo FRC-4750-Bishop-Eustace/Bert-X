067a946a47004d8e2958206628a681cef24e6c59
dan P <danielpignuola24@eustace.org>
2022-02-15 19:29:26 -0500

    dan Add btn press framework

---
+++ src/main/java/frc/robot/Commands/Button.java
@@ -0,0 +1,16 @@
+package frc.robot.Commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class Button {
+
+    public Button() {
+
+    }
+    
+    public void ButtonPress(){
+        
+    }
+
+
+}
