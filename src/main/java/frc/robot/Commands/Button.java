f687c420065ea9f2f8d50a101f2496933d92ac3e
Emerson Palaganas <emersonpalaganas@gmail.com>
2022-02-18 18:00:01 -0500

    Auto stash before merge of "Emerson_B" and "origin/master"

---
+++ src/main/java/frc/robot/Commands/Button.java
@@ -0,0 +1,22 @@
+package frc.robot.Commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+
+DigitalInput toplimitSwitch = new DigitalInput(0);
+DigitalInput bottomlimitSwitch = new DigitalInput(1);
+PWMVictorSPX motor = new PWMVictorSPX(0);
+Joystick joystick = new Joystick(0);
+
+public class Button {
+
+    public Button() {
+
+    }
+    
+    public void ButtonPress(){
+        
+    }
+
+
+}
