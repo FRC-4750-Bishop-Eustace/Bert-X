acbd7d400c34e145d08d9050c7e25fb6b74d7516
Emerson936 <57594164+Emerson936@users.noreply.github.com>
2022-02-26 15:10:26 -0500

    Update OI.java

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -7,22 +7,22 @@ package frc.robot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilipj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import frc.auto.DriveToDistance;
-import frc.commands.Indexer;
-import frc.commands.ResetIndexer;
+//import frc.auto.DriveToDistance;
+//import frc.commands.Indexer;
+//import frc.commands.ResetIndexer;
 //import frc.commands.ReverseDriveTrain;
-import frc.commands.ReverseIntake;
-import frc.commands.RunClimber;
+//import frc.commands.ReverseIntake;
+//import frc.commands.RunClimber;
 //import frc.commands.RunIntestines;
 //import frc.commands.Shoot;
-import frc.commands.RunIntake;
-import frc.commands.RunShooter;
+//import frc.commands.RunIntake;
+//import frc.commands.RunShooter;
 //import frc.commands.ShootDisable;
 //import frc.commands.ShootEnable;
 //import frc.commands.SlowModeDriveTrain;
-import frc.commands.StopClimber;
-import frc.commands.StopIntake;
-import frc.commands.StopShooter;
+//import frc.commands.StopClimber;
+//import frc.commands.StopIntake;
+//import frc.commands.StopShooter;
 //import frc.subsystems.ShooterSubsystems;
 
 /** Add your docs here. */
@@ -36,40 +36,40 @@ public class OI {
     JoystickButton toggleDirection = new JoystickButton(driveStick, 3);
     //JoystickButton shootButton = new JoystickButton(driveStick, 6);
    // JoystickButton stopShooting = new JoystickButton(driveStick, 5);
-    JoystickButton driveStickRunClimber = new JoystickButton(driveStick, 4);
-    JoystickButton slowDriveMode = new JoystickButton(driveStick, 2);
+    //JoystickButton driveStickRunClimber = new JoystickButton(driveStick, 4);
+    //JoystickButton slowDriveMode = new JoystickButton(driveStick, 2);
     //edu.wpi.first.wpilibj2.command.button.JoystickButton runFeeder = new edu.wpi.first.wpilibj2.command.button.JoystickButton(driveStick, 7);
-    JoystickButton noPIDShooterButton = new JoystickButton(driveStick, 1);
+    //JoystickButton noPIDShooterButton = new JoystickButton(driveStick, 1);
     
     //controller buttons
-    JoystickButton runIntake = new JoystickButton(controller, 8);
-    JoystickButton runIndexer = new JoystickButton(controller, 7);
-    JoystickButton controllerRunClimber = new JoystickButton(controller, 2);
-    JoystickButton resetIndexer = new JoystickButton(controller, 1);
-    JoystickButton reverseIntake = new JoystickButton(controller, 3);
+    //JoystickButton runIntake = new JoystickButton(controller, 8);
+    //JoystickButton runIndexer = new JoystickButton(controller, 7);
+    //JoystickButton controllerRunClimber = new JoystickButton(controller, 2);
+    //JoystickButton resetIndexer = new JoystickButton(controller, 1);
+    //JoystickButton reverseIntake = new JoystickButton(controller, 3);
     
-    JoystickButton autoTest = new JoystickButton(controller ,5);
+    //JoystickButton autoTest = new JoystickButton(controller ,5);
     
     public OI(){
         //buttons on the drivestick
-        //toggle Direction.whenReleased(new ReverseDriveTrain());
-        //slowDriveMode.whenReleased(new SlowModeDriveTrain());
+        toggle Direction.whenReleased(new ReverseDriveTrain());
+        slowDriveMode.whenReleased(new SlowModeDriveTrain());
         //shootButton.whenPressed(new ShootEnable());
         //stopShooting.whenPressed(new ShootDisable());
-        noPIDShooterButton.whileHeld(new RunShooter());
-        noPIDShooterButton.whenReleased(new StopShooter());
+        //noPIDShooterButton.whileHeld(new RunShooter());
+        //noPIDShooterButton.whenReleased(new StopShooter());
         
         //buttons on the controller
-        runIntake.whileHeld(new RunIntake());
-        runIntake.whenReleased(new StopIntake());
-        runIndexer.whileHeld(new Indexer());
-        resetIndexer.whenPressed(new ResetIndexer());
-        controllerRunClimber.whileHeld(new RunClimber());
-        controllerRunClimber.whenReleased(new stopClimber());
-        reverseIntake.whileHeld(new ReverseIntake());
-        reverseIntake.whenReleased(new StopIntake());
+        //runIntake.whileHeld(new RunIntake());
+        //runIntake.whenReleased(new StopIntake());
+        //runIndexer.whileHeld(new Indexer());
+        //resetIndexer.whenPressed(new ResetIndexer());
+        //controllerRunClimber.whileHeld(new RunClimber());
+        //controllerRunClimber.whenReleased(new stopClimber());
+        //reverseIntake.whileHeld(new ReverseIntake());
+        //reverseIntake.whenReleased(new StopIntake());
         
         // test button so we don't worry about auto failing
-        autoTest.whenReleased(new DriveToDistance());
+        //autoTest.whenReleased(new DriveToDistance());
     }
 }
