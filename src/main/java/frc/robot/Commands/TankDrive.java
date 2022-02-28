022e37ac75125dc1a3fb0ba20f7f8ef9d6223441
Emerson936 <57594164+Emerson936@users.noreply.github.com>
2022-02-28 18:10:31 -0500

    Update TankDrive.java

--- src/main/java/frc/robot/Commands/TankDrive.java
+++ src/main/java/frc/robot/Commands/TankDrive.java
@@ -9,20 +9,23 @@ import frc.robot.Robot;
 public class TankDrive extends Command [
   
   public TankDrive() {
-    // Require the drive train
+    // Require the drive train or wont work
     requires(Robot.driveTrain);
   }
   
   @Override
   protected void execute() {
     // Pass the joystick values to joystickDrive()
+    // if the drivetrain is in reverese and in slowmode, the robot will get the joystick input and sensitivity and will tell the person driving that
+    // the robot is in reverse and slowmode
     if (!Robot.driveTrain.reversed()) {
       if (!Robot.driveTrain.slowMode()){
       Robot.driveTrain.joystickDrive(-OI.driveStick.getY() / 2, OI.driveStick.getThrottle() / 3);
- //     System.out.println("slowMode and reverse are on");
+      System.out.println("slowMode and reverse are on");
       } else {
+        // This is the same thing as above except the robot is only in reverse and not in slowmode.
         Robot.driveTrain.joystickDrive(-OI.driveStick.getY(), OI.driveStick.getThrottle());
-       // System.out.println("Reverse is on");
+        System.out.println("Reverse is on");
       }
     } else {
       if(!Robot.driveTrain.slowMode()){
