1078b7ac10a90524037550ff038572fbdb0c081d
KMonari <monari1213@gmail.com>
2022-03-18 18:40:42 -0400

    Remove WIP code in OI

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -10,11 +10,6 @@ import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 public class OI {
 
     public static Joystick driveStick = new Joystick(RobotMap.DRIVESTICK_PORT);
-    public static Joystick controller = new Joystick(RobotMap.CONTROLSTICK_PORT);
-
-    //The intake wheel will spin if and only if this button is held.
-    JoystickButton intakeButton = new JoystickButton(controller, RobotMap.INTAKE_BUTTON_ID);
-    JoystickButton reverseButton = new JoystickButton(controller, RobotMap.REVERSE_SYSTEMS_BUTTON_ID);
 
     public OI(){ 
     }
