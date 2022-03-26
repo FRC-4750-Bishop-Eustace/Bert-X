35b363ee2efa7a5cfb23d006ac63061e32e9350f
KMonari <monari1213@gmail.com>
2022-03-26 09:53:22 -0400

    fixed port and robot drives

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,7 +8,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
+//import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.Subsystem.Drivetrain;
 import frc.robot.Subsystem.Intake;
 
--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -13,13 +13,13 @@ public class RobotMap {
   public static final int CONTROLSTICK_PORT = 1;
 
     //Drivetrain motors ID (Cans)
-    public static final int LEFT_FRONT_ID = 14;
-    public static final int LEFT_BACK_ID = 15;
-    public static final int RIGHT_FRONT_ID = 2;
-    public static final int RIGHT_BACK_ID = 1;
+    public static final int LEFT_FRONT_ID = 3;
+    public static final int LEFT_BACK_ID = 2;
+    public static final int RIGHT_FRONT_ID = 1;
+    public static final int RIGHT_BACK_ID = 0;
     
     //Intake motor ID
-    public static final int INTAKE_MOTOR_ID = 8;
+    public static final int INTAKE_MOTOR_ID = 12;
 
     //Joystick Button IDs
     public static final int INTAKE_BUTTON_ID = 1;
