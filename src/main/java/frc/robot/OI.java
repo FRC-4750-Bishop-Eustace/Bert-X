783e444ddd5f469a584e2412b7e01f18b886e14f
KMonari <monari1213@gmail.com>
2022-03-26 14:36:22 -0400

    Uncomment the reverseIntake button.

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -28,7 +28,7 @@ public class OI {
     //The intake wheel will spin if and only if this button is held.
     public JoystickButton intakeButton = new JoystickButton(controller, RobotMap.INTAKE_BUTTON_ID);
     public JoystickButton shooterButton = new JoystickButton(controller, RobotMap.SHOOTER_HEAD_BUTTON_ID);
-    // public JoystickButton reverseIntakeButton = new JoystickButton(controller, RobotMap.REVERSE_INTAKE_BUTTON_ID);
+    public JoystickButton reverseIntakeButton = new JoystickButton(controller, RobotMap.REVERSE_INTAKE_BUTTON_ID);
     // public JoystickButton reverseShooterButton = new JoystickButton(controller, RobotMap.REVERSE_SHOOTER_HEAD_BUTTON_ID);    
 
     public JoystickButton belt1Button = new JoystickButton(controller, RobotMap.BELT_1_BUTTON_ID);
